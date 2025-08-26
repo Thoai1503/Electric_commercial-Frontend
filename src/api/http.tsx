d043ec2551c7a5aaf8417e7418cec04797bfc661
Thoai1503 <vothoai1503@gmail.com>
2025-08-26 11:15:05 +0700

    config url

--- src/api/http.tsx
+++ src/api/http.tsx
@@ -3,10 +3,10 @@ import axios from "axios";
 //const API_URL_BASE = import.meta.env.VITE_API_URL;
 
 export const Request = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://electric-commercial-node-js-backend.vercel.app/",
 });
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://electric-commercial-node-js-backend.vercel.app/",
   withCredentials: true,
 });
 
