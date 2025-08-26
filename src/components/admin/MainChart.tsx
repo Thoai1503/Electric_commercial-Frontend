6c72dc4a95ee0ca5e9dda24060626afbe5de2a24
Thoai1503 <vothoai1503@gmail.com>
2025-08-26 22:53:18 +0700

    fix for deployment

--- src/components/admin/MainChart.tsx
+++ src/components/admin/MainChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { CChartLine } from "@coreui/react-chartjs";
 import { getStyle } from "@coreui/utils";
 import { Chart } from "chart.js";
