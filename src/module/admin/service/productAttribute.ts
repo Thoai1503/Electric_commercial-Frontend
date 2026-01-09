253ab6ad3ca49012fa0d29ed38c51e708c2cb365
Thoai1503 <vothoai1503@gmail.com>
2026-01-09 18:44:16 +0700

    rebuild 2

--- src/module/admin/service/productAttribute.ts
+++ src/module/admin/service/productAttribute.ts
@@ -1,11 +1,11 @@
-import { catalogRequestTesting } from "../../../api/http";
+import { catalogRequest } from "../../../api/http";
 import type { ProductAttribute } from "../../../type/ProductAttribute";
 
 export const updateProductAttribute = async (
   id: number,
   en: Partial<ProductAttribute>
 ): Promise<boolean> => {
-  return await catalogRequestTesting
+  return await catalogRequest
     .put(`/productattribute/${id}`, en)
     .then((res) => res.data)
     .catch((error) => {
